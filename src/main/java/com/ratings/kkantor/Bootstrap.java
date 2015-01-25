25b7172f95a03629f441b1513e8c8302617b26ef
kodykantor <kody.kantor@gmail.com>
2015-01-25 15:26:42 -0600

    Add test Spark endpoint

--- src/main/java/com/ratings/kkantor/Bootstrap.java
+++ src/main/java/com/ratings/kkantor/Bootstrap.java
@@ -11,11 +11,16 @@ public class Bootstrap {
     private static final Logger logger = LogManager.getLogger(Bootstrap.class);
 
     public static void main(String[] args) {
+        get("/test", (request, response) -> doStuff());
+    }
+    public static String doStuff() {
+        logger.debug("Doing stuff");
         GameServiceImpl rs = new GameServiceImpl();
         VotingServiceImpl vs = new VotingServiceImpl();
         logger.info("Game added? " + rs.addGame("Kody's awesome game", true));
         logger.info("Voted. New score is " + vs.changeVote(true, "Kody's Awesome Game"));
         logger.info("Game ID is " + rs.findGame("Kody's awesome game"));
         logger.info("Deleted game and votes? " + rs.deleteGame("Kody's awesome game"));
+        return "Hello!";
     }
 }
